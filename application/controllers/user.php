10c9a2f7a243912e527a109bbea3761829731db2
verdianp <verdpoundra18@gmail.com>
2018-04-25 10:11:00 +0700

    update sprint_3

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -2,7 +2,7 @@
 
 class User extends CI_Controller {
   function add(){
-    echo $this->input->post("username");
+    echo $this->input->post("username", "password", "fullname");
   }
   function form(){
     $this->load->view("form_user_v");
