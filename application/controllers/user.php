8d0f23d050b92cf06658d44693e635f86560afbd
verdianp <verdnpoundra18@gmail.com>
2018-04-26 14:55:47 +0700

    update baru popon

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -2,7 +2,10 @@
 
 class User extends CI_Controller {
   function add(){
-    echo $this->input->post("username", "password", "fullname");
+    echo $this->input->post("username")."<br/>";
+    echo $this->input->post("password")."<br/>";
+    echo $this->input->post("fullname")."<br/>";
+    echo $this->input->post("level")."<br/>";
   }
   function form(){
     $this->load->view("form_user_v");
--- application/views/form_user_v.php
+++ application/views/form_user_v.php
@@ -1,29 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Input Data</title>
+  <title>Input Data</title>
 </head>
 <body>
-	<form action="http://localhost/OK-Travel/index.php/user/add" method="post">
-	<table>
-	<tr>
-		<td>Username</td>
-		<td>:</td>
-		<td><input name="username" value=""></td></tr>
-	<tr>
-		<td>Pass</td>
-		<td>:</td>
-		<td><input name="password" type="password" value=""></td></tr>
-	<tr>
-		<td>Fullname</td>
-		<td>:</td>
-		<td><input name="fullname" value=""></td></tr>
-	<tr>
-		<td>Level</td><td>:</td>
-		<td><input name="tombol" value=""></td></tr>
-		<tr><td><input type="submit" name="tombol" value="simpan"></td>
-	</tr>
-	</table>
+	<form action="http://localhost/OK-Travel/index.php/user/add" method="POST">
+		<table>
+			<tr><td>Username</td><td> : </td><td><input name="username" type="text"></td></tr>
+			<tr><td>Password</td><td> : </td><td><input name="password" type="password"></td></tr>
+			<tr><td>Fullname</td><td> : </td><td><input name="fullname" type="text"></td></tr>	
+			<tr><td>Level</td><td>:</td><td><select name="level">
+				<option value="admin">Admin</option>
+				<option value="user">User</option></select></td></tr>
+			<tr><td><input name="tombol" type="submit" value="Simpan"></td></tr>
+		</table>
 	</form>
 </body>
 </html>
